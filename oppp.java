1411568ac453251d3678212dd29dff9fbd4a145f
Oni Sondegau <146151301+OniSondegau@users.noreply.github.com>
2023-09-27 10:19:56 +0800

    Add files via upload
    Operator

---
+++ oppp.java
@@ -0,0 +1,10 @@
+package Praktikum;
+
+public class oppp {
+  public static void main(String[] args) {
+    int bill1, bill2;
+    bill1 = 8;
+    bill2 = 5;
+   System.out.println(bill1 * bill2);
+  }
+}
